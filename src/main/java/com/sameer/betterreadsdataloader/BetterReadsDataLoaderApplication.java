41f2757d3640f397d6c7c3e80a80ed40f8ed0fdb
shaiksameer <sameershaik2001@yahoo.com>
2022-01-18 08:08:54 +0530

    completed properties setup

--- src/main/java/com/sameer/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/sameer/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -1,13 +1,25 @@
 package com.sameer.betterreadsdataloader;
 
+import com.sameer.betterreadsdataloader.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsDataLoaderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
     }
 
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 }
