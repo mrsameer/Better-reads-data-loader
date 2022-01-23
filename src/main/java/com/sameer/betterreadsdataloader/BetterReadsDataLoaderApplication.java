7c5b752d0089a53f9bdadb8e297eea596a238c59
shaiksameer <sameershaik2001@yahoo.com>
2022-01-23 13:30:47 +0530

    exported works datadump to astra

--- src/main/java/com/sameer/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/sameer/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -1,9 +1,11 @@
 package com.sameer.betterreadsdataloader;
 
-import com.fasterxml.jackson.core.io.JsonEOFException;
 import com.sameer.betterreadsdataloader.author.Author;
 import com.sameer.betterreadsdataloader.author.AuthorRepository;
+import com.sameer.betterreadsdataloader.book.Book;
+import com.sameer.betterreadsdataloader.book.BookRepository;
 import com.sameer.betterreadsdataloader.connection.DataStaxAstraProperties;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +15,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -29,6 +35,9 @@ public class BetterReadsDataLoaderApplication {
 
     @Autowired
     AuthorRepository authorRepository;
+
+    @Autowired
+    BookRepository bookRepository;
 
     @Value("${datadump.location.author}")
     private String authorDumpLocation;
@@ -67,12 +76,73 @@ public class BetterReadsDataLoaderApplication {
     }
 
     private void initWorks() {
+        Path path = Paths.get(worksDumpLocation);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(line -> {
+                // Read and parse the line
+                String jsonString = line.substring(line.indexOf("{"));
 
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+
+                    // Construct Book object
+                    Book book = new Book();
+                    book.setId(jsonObject.getString("key").replace("/works/", ""));
+                    book.setName(jsonObject.optString("title"));
+
+                    JSONObject descriptionObject = jsonObject.optJSONObject("description");
+                    if (descriptionObject != null) {
+                        book.setDescription(descriptionObject.optString("value"));
+                    }
+
+                    JSONObject publishedObject = jsonObject.optJSONObject("created");
+                    if (publishedObject != null) {
+                        String dateStr = publishedObject.getString("value");
+                        book.setPublishedDate(LocalDate.parse(dateStr, dateTimeFormatter));
+                    }
+
+                    JSONArray coversJsonArray = jsonObject.optJSONArray("covers");
+                    if (coversJsonArray != null) {
+                        List<String> coverIds = new ArrayList<>();
+                        for (int i = 0; i < coversJsonArray.length(); i++) {
+                            coverIds.add(coversJsonArray.getString(i));
+                        }
+                        book.setCoverIds(coverIds);
+                    }
+
+                    JSONArray authorsJsonArray = jsonObject.optJSONArray("authors");
+                    if (authorsJsonArray != null) {
+                        List<String> authorIds = new ArrayList<>();
+                        for (int i = 0; i < authorsJsonArray.length(); i++) {
+                            String authorId = authorsJsonArray.getJSONObject(i)
+                                    .getJSONObject("author")
+                                    .getString("key")
+                                    .replace("/authors/", "");
+                            authorIds.add(authorId);
+                        }
+                        book.setAuthorIds(authorIds);
+                        List<String> authorNames = authorIds.stream().map(id -> authorRepository.findById(id))
+                                .map(optionalAuthor -> {
+                                   if (!optionalAuthor.isPresent()) return "Unknown Author";
+                                    return optionalAuthor.get().getName();
+                                })
+                                .collect(Collectors.toList());
+                        book.setAuthorNames(authorNames);
+                    }
+                    bookRepository.save(book);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @PostConstruct
     public void start() {
-        initAuthors();
+        //initAuthors();
         initWorks();
 
     }
